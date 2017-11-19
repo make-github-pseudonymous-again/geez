7730ea2d95ab3ed39464ae55ed661e6d143ce5f2
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-11-19 21:07:31 +0100

    allow and git command or alias

--- src/cli.js
+++ src/cli.js
@@ -9,16 +9,35 @@ const cwd = '.' ;
 
 const args = process.argv.slice(2);
 
-if ( args.length !== 1 ) {
-	console.error('usage: geez {pull|push}');
+if ( args.length < 1 ) {
+	console.error('usage: geez <git command> [<git command argument>, ...]');
 	process.exit(2);
 }
-const available_commands = [ 'pull' , 'push' ] ;
+
+let available_commands = [ ] ;
+
+let result = execa.sync('git', ['config', '-l']);
+let lines = result.stdout.split('\n');
+lines = lines.filter( line => line.startsWith('alias.') );
+lines = lines.map( line => line.split('=')[0] );
+lines = lines.map( line => line.split('.')[1] );
+available_commands = available_commands.concat(lines);
+
+result = execa.sync('git', ['help', '-a']);
+lines = result.stdout.split('\n');
+lines = lines.filter( line => line.match(/^  [a-z]/) );
+lines = lines.map( line => line.split(' ') );
+for ( const line of lines ) {
+	available_commands = available_commands.concat(line.filter(line => line));
+}
+
+available_commands.sort();
 
 const cmd = args[0] ;
 
 if ( ! available_commands.includes(cmd) ) {
 	console.error(`Unknown command '${cmd}'.`);
+	console.error(`Available commands are: ${available_commands.join(', ')}.`);
 	process.exit(2);
 }
 
@@ -28,12 +47,12 @@ const tasks = new Listr([
 		task: ctx => repos_promise(cwd).then( repos => { ctx.repos = repos ; } )
 	},
 	{
-		title: `${cmd[0].toUpperCase()}${cmd.substring(1)}ing repos`,
+		title: `Applying 'git ${args.join(' ')}' to all repos`,
 		task: ctx => {
 
 			const pullingtasks = ctx.repos.map(repo => ({
 				title: repo,
-				task: () => execa('git', ['-C', repo, cmd])
+				task: () => execa('git', ['-C', repo].concat(args))
 			}));
 
 			return new Listr(pullingtasks, {
